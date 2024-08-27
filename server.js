aa756e7387d4e85812d878d45506ef400e34b2e6
João Bispo <jsbispo1101@gmail.com>
2024-08-27 08:28:08 -0400

    pasta todos corrigida

--- server.js
+++ server.js
@@ -239,10 +239,7 @@ app.post("/deletar-pasta", (req, res) => {
 app.post("/add-pasta", (req, res) => {
     db.query(
         "INSERT INTO journalpastasfs (nomepasta, user_id) VALUES ($1, $2)",
-        [
-            req.body.novapasta.toLowerCase().trim() || "Todos",
-            req.session.usuario.id,
-        ]
+        [req.body.novapasta.toLowerCase().trim(), req.session.usuario.id]
     );
     return res.redirect("/logado");
 });
@@ -256,7 +253,7 @@ app.post("/add", (req, res) => {
             "Desconhecido",
             new Date(),
             req.session.usuario.id,
-            pastaAtual,
+            pastaAtual || "Todos",
         ]
     );
 
